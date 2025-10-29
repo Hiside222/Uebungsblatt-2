06eaf4b0effc520fc4b336038a9f8bdf3091e5ef
Bilal Najjar <bilalnajjar04@gmail.com>
2025-10-29 13:08:39 +0100

    praesenz aufgabe 2 und 3

--- src/p2/P2_main.java
+++ src/p2/P2_main.java
@@ -1,7 +1,15 @@
 package p2;
 
 public class P2_main {
-int  a ;
-boolean b ;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 1;
+		boolean b = false;
+		if (i > 10) { 
+			b =true;
+		}
+		System.out.println("i hat den Wert: " + i + " und b hat den Wert: " + b);
+	}
 
 }
